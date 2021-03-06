a961150f4844c2a40490e9cf0fe7a555402f1c2b
Max Ribbans <ribbans.max@gmail.com>
2021-03-06 12:50:31 -0500

    Simplecov setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,9 +11,17 @@
 # a separate helper file that requires the additional dependencies and performs
 # the additional setup, and require it from the spec files that actually need
 # it.
-#
+require 'simplecov'
+SimpleCov.start
+SimpleCov.add_filter %W[
+  app/helpers
+  config
+  spec
+]
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  config.include Warden::Test::Helpers
+  Warden.test_mode!
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
