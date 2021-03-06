bd794d0521a7ef36102850bad99ab2a60bb64cd2
Max Ribbans <ribbans.max@gmail.com>
2021-03-06 12:51:22 -0500

    Remove warden

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,8 +20,6 @@ SimpleCov.add_filter %W[
 ]
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  config.include Warden::Test::Helpers
-  Warden.test_mode!
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
